0b1a5c83c52e479cb60ea360725632a0792ce51c
Alie Stone <helloaliestone@gmail.com>
2020-12-14 20:56:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,13 @@
 class Application
 
-  @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      @@items.each do |item|
+    if req.path.match(/cart/)
+      @@cart.each do |item|
         resp.write "#{item}\n"
       end
     elsif req.path.match(/search/)
